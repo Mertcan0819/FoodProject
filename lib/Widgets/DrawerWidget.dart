c1a18e9edff4cf336bdb352632338488775299cc
Mert Can <123500356+Mertcan0819@users.noreply.github.com>
2023-12-18 16:26:06 +0300

    Update DrawerWidget.dart

--- lib/Widgets/DrawerWidget.dart
+++ lib/Widgets/DrawerWidget.dart
@@ -60,17 +60,7 @@ class DrawerWidget extends StatelessWidget {
               ),
              ),
 
-              Card(
-               child: ListTile(                                     // (bu card tasarımını komle bitirdikten sontra listTile ctrl . ile yeni bir widget kazandırıp bunun dına da card veriyoruz bu sayede görünümü dha güzel oluyor ve sektörde böyle kullanılıyor )   anlamasına cad tasarımlarında kullanılır ilerde çok çok kullanıcaz proje geliştirmelerde 
-                title: Text('My Card'),                             // burada video da text yerine img kullanıyor veli hoca. bizde kullanabiliriz. fakat tasarım açısından şuan böyle bırakıyorum 
-                subtitle: Text('how do you your card'),           // textin altına yazı yazmak içib subtitle yapmamız lazım. Alt başlık manasında 
-                leading:  Icon(Icons.attach_money),              // bu para işleminin sol tarafına bir şey koymak istiyorsan leading yapmalıyız. icon koydum veya başka şyler koyularabilir işleme göre 
-                trailing: Icon(Icons.chevron_right_outlined),   // buda sağ tarafa bir şey koyulması için. trailing yazıyoruz 
-                dense: true,                                          // buda kompanenti olabldiğince sıkıştırmayı sağlıyor
-                contentPadding: EdgeInsets.zero ,                    // bu gözden kaçabilir fakat çök önemi. yapının içinde ki paddingi sıfırlıyoruz ki colm a yani ana widgeta verdiğimiz paddingi bozmamak için. yukarıdan aşagı inen kompanentleri bozmamak için 
-                                                       // buda yaptıgımız kartı tıklanabilir hale getirmemizi sağlıyor artık tıklayabiliyoruz. fakat column yanındaki const u silmemiz lazım yoksa hata alırız 
-              ),
-             ),
+            
             
         ]
       ),
